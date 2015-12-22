56c874ee1512a210fa1a3aa7686ab7896d9d6a6b
AAZ H <aazhbd@yahoo.com>
2015-12-22 16:46:40 +0000

    General configuration changes

--- conf.php
+++ conf.php
@@ -6,7 +6,7 @@ $conf = array(
         'development_mode' => false
     ),
     'development' => array(
-        'db_name' => 'dbname',
+        'db_name' => 'artcmsdb',
         'db_host' => 'localhost',
         'db_user' => 'root',
         'db_pass' => '',
