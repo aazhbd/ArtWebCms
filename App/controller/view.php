e90cdefd1529eff3e4f6f92dcb299794349a4d2d
AAZ H <aazhbd@yahoo.com>
2016-12-23 21:29:42 +0100

    Minor Refactoring

--- App/controller/view.php
+++ App/controller/view.php
@@ -81,6 +81,7 @@ class Views extends Controller
 
         if ($article) {
             $app->setTemplateData(array(
+                'page_title' => $article['title'],
                 'title' => $article['title'],
                 'subtitle' => $article['subtitle'],
                 'body' => $parsemd->text(stripslashes(html_entity_decode(htmlentities($article['body'])))),
