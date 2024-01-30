f66390fe5e55ababd9dcabce56c945d19e80d293
Abdullah Al Zakir Hossain <aazhbd@yahoo.com>
2024-01-30 21:50:36 +0100

    Added containerization and updated code support for PHP 8.3

--- conf.php
+++ conf.php
@@ -6,10 +6,10 @@ $conf = array(
         'development_mode' => false
     ),
     'development' => array(
-        'db_name' => 'artcmsdb',
-        'db_host' => 'localhost',
-        'db_user' => 'root',
-        'db_pass' => 'root',
+        'db_name' => 'artcms_db',
+        'db_host' => 'data-service',
+        'db_user' => 'artcmsuser',
+        'db_pass' => 'tpass',
         'development_mode' => true,
         'path_sys_template' => '/Template/base.twig',
         'path_static' => '/Template/static/',
